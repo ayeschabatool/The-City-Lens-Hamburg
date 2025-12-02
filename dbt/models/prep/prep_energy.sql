0efa97abdf851c33ac41409448b616ae5e6d54de
Kiien Kyonh Do <dosasha244@gmail.com>
2025-12-02 10:44:59 +0000

    files edited

--- dbt/models/prep/prep_energy.sql
+++ dbt/models/prep/prep_energy.sql
@@ -1,29 +1,29 @@
 with consumed as (
     select
         cast(year as integer) as year,
-        cast(replace("consumed_energie_GWh", ',', '.') as float) as consumed_energie_gwh
-    from {{ ref('stg_consumed_energie_GWh') }}
+        cast(replace(consumed_energie_gwh, ',', '.') as float) as consumed_energie_gwh
+    from {{ ref('stg_consumed_energie') }}
 ),
 
 generated as (
     select
         cast(year as integer) as year,
-        cast(replace("generated_energie_GWh", ',', '.') as float) as generated_energie_gwh
-    from {{ ref('stg_generated_energie_GWh') }}
+        cast(replace(generated_energie_gwh, ',', '.') as float) as generated_energie_gwh
+    from {{ ref('stg_generated_energie') }}
 ),
 
 total_feed as (
     select
         cast(year as integer) as year,
-        cast(replace("total_feed_in_GWh", ',', '.') as float) as total_feed_in_gwh
-    from {{ ref('stg_total_feed_in_GWh') }}
+        cast(replace(total_feed_in_gwh, ',', '.') as float) as total_feed_in_gwh
+    from {{ ref('stg_total_feed_in') }}
 ),
 
 renewable_feed as (
     select
         cast(year as integer) as year,
-        cast(replace("renewable_feed_in_GWh", ',', '.') as float) as renewable_feed_in_gwh
-    from {{ ref('stg_renewable_feed_in_GWh') }}
+        cast(replace(renewable_feed_in_gwh, ',', '.') as float) as renewable_feed_in_gwh
+    from {{ ref('stg_renewable_feed_in') }}
 )
 
 select
@@ -36,4 +36,4 @@ from consumed c
 left join generated g on c.year = g.year
 left join total_feed t on c.year = t.year
 left join renewable_feed r on c.year = r.year
-order by c.year
+order by c.year;
